4a6057178589fd5744331a4f243278fe0e00cce7
Tobias Gottschalk <gottschalk.tobias98@outlook.de>
2024-08-12 12:35:31 +0200

    added util: isIPv4Address.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello World!");
+import { isIPv4Address } from "./utils/isIPv4Address";
+
+const testAddresses =   [
+                            '192.168.1.1',
+                            '255.255.255.255',
+                            '0.0.0.0',
+                            '256.256.256.256',
+                            '192.168.1.1.1',
+                            'abc.def.ghi.jkl'
+                        ];
+
+testAddresses.forEach(address => {
+    console.log(`${address} is ${isIPv4Address(address) ? 'a VALID' : 'an INVALID'} IPv4 address.`);
+});
